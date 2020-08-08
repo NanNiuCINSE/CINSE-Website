4b99c2d14ce59c19c70079d0c0a147c9b68ae95b
Sumit <ghewdesumit@gmail.com>
2020-08-07 22:06:33 -0400

    created the project and landing page component

---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+	return (
+		<Fragment>
+			<div className='logo-container'></div>
+			<div className='navigation'>
+				<div className='navigation-tabs'></div>
+			</div>
+		</Fragment>
+	);
+};
+
+export default LandingPage;
