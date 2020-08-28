f26d187ec032fd9358011a4bb681b005c0186f99
Sumit <ghewdesumit@gmail.com>
2020-08-28 16:26:49 -0400

    feat: created header with hover effect

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -6,7 +6,13 @@ const LandingPage = () => {
 		<Fragment>
 			<div className='logo-container'></div>
 			<div className='navigation'>
-				<div className='navigation-tabs'></div>
+				<div className='navigation-tabs'>Home</div>
+				<div className='navigation-tabs'>Members</div>
+				<div className='navigation-tabs'>Projects</div>
+				<div className='navigation-tabs'>News</div>
+				<div className='navigation-tabs'>Publications</div>
+				<div className='navigation-tabs'>Data/Tools</div>
+				<div className='navigation-tabs'>Contact Us</div>
 			</div>
 		</Fragment>
 	);
