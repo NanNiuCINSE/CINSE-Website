3a09d6d2ef79fd06cdfe5b5d19da0b23defbbdb7
Sumit <ghewdesumit@gmail.com>
2020-09-09 09:55:50 -0400

    created the profile data file, to create separation of concern

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,30 +1,26 @@
 import React from 'react';
 import './Profile.css';
-import professor from '../../resources/Images/nan.jpg';
+import * as ProfileData from './Profile.data';
 
 const Profile = () => {
+	const { profileData } = ProfileData;
 	return (
 		<div className='profile-main-container'>
 			<header className='profile-header'>
 				<div className='profile-holder'>
-					<img className='profile-img' src={professor} alt='professor' />
+					<img
+						className='profile-img'
+						src={profileData.imageSource}
+						alt='professor'
+					/>
 					<div className='prof-info'>
-						<p className='profile-name'>Prof. Nan Niu</p>
-						<p className='profile-subtitle'>Computer Science & Informatics</p>
+						<p className='profile-name'>{profileData.name}</p>
+						<p className='profile-subtitle'>{profileData.title}</p>
 					</div>
 				</div>
 			</header>
 			<div className='profile-container'>
-				<div className='profile-info'>
-					Nan Niu is the Director of the Software Engineering Research
-					Laboratory in the Department of Electrical Engineering and Computer
-					Science (EECS) at the University of Cincinnati (UC). His current
-					research interests focus on requirements engineering, model-driven
-					engineering, scientific software development, and human-centric
-					computing. He and his research group are tackling a variety of
-					projects concerning the quality of the software and the productivity
-					of the people who are building software.
-				</div>
+				<div className='profile-info'>{profileData.description}</div>
 			</div>
 		</div>
 	);
