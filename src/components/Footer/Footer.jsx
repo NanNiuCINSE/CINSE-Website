fa1aea27534bcc2bb9590db5e3958fff779fbe5b
Sumit <ghewdesumit@gmail.com>
2020-09-09 10:48:25 -0400

    created data files for the news section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,17 +13,21 @@ const Footer = () => {
 				<p>Cincinnati, Ohio 45221-0030</p>
 				<p>Phone: +1 513 556 0051</p>
 				<div className='footer-links'>
-					<a href='#'>Alerts</a>
-					<a href='#'>Clery and HEOA Notice</a>
-					<a href='#'>Notice of Non-Descrimination</a>
+					<a href='https://www.uc.edu/alert.html'>Alerts</a>
+					<a href='https://www.uc.edu/about/publicsafety/clery/annual-security-report.html'>
+						Clery and HEOA Notice
+					</a>
+					<a href='http://uc.edu/about/policies/non-discrimination.html'>
+						Notice of Non-Descrimination
+					</a>
 					<a href='#'>eAccessibility Concern</a>
 					<a href='#'>Copyright Information</a>
 					<p> &#169; 2020</p>
-					<a href='#'> University of Cincinnati</a>
+					<a href='https://www.uc.edu/'> University of Cincinnati</a>
 				</div>
 				<p className='footer-last-link'>
 					This website offers many PDF files for download, which require{' '}
-					<a href='#'> Adobe Reader </a> to view.
+					<a href='https://get.adobe.com/reader/'> Adobe Reader </a> to view.
 				</p>
 			</div>
 		</div>
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,45 +1,23 @@
 import React from 'react';
 import './News.css';
 
+import * as NewsData from './News.data';
 const News = () => {
+	const { newsData, newsHeader } = NewsData;
+	const newsItems = newsData.slice(0, 5).map((item, index) => {
+		return (
+			<li className='news-item' key={index}>
+				{item.news}
+			</li>
+		);
+	});
+
 	return (
 		<aside className='news-section'>
 			<div className='news-container'>
-				<header className='news-header'>Recent News</header>
+				<header className='news-header'>{newsHeader}</header>
 				<div className='news'>
-					<ul>
-						<li className='news-item'>
-							Lorem Ipsum is simply dummy text of the printing and typesetting
-							industry. Lorem Ipsum is simply dummy text of the printing and
-							typesetting industry.
-						</li>
-						<li className='news-item'>
-							Lorem Ipsum is simply dummy text of the printing and typesetting
-							industry. Lorem Ipsum is simply dummy text of the printing and
-							typesetting industry.
-						</li>
-						<li className='news-item'>
-							Lorem Ipsum is simply dummy text of the printing and typesetting
-							industry. Lorem Ipsum is simply dummy text of the printing and
-							typesetting industry.
-						</li>
-						<li className='news-item'>
-							Lorem Ipsum is simply dummy text of the printing and typesetting
-							industry.
-						</li>
-						<li className='news-item'>
-							Lorem Ipsum is simply dummy text of the printing and typesetting
-							industry.
-						</li>
-						<li className='news-item'>
-							Lorem Ipsum is simply dummy text of the printing and typesetting
-							industry.
-						</li>
-						<li className='news-item'>
-							Lorem Ipsum is simply dummy text of the printing and typesetting
-							industry.
-						</li>
-					</ul>
+					<ul>{newsItems}</ul>
 				</div>
 			</div>
 		</aside>
