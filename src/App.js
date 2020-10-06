eff366040d0b3ce4cfd4d9e9481a80343da7fb56
Sumit <ghewdesumit@gmail.com>
2020-10-06 10:31:40 -0400

    added the footer to every page and corrected the css of the member page and footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import Header from './components/Header/Header';
 import LandingPage from './components/LandingPage/LandingPage';
 import Members from './components/Members/Members';
+import Footer from './components/Footer/Footer';
 import { Switch, Route } from 'react-router-dom';
+
 // import Route from 'react-router-dom/Route';
 
 const App = () => {
@@ -11,11 +13,11 @@ const App = () => {
 		<div className='main-container'>
 			<div className='logo-container'></div>
 			<Header />
-
 			<Switch>
 				<Route path='/' component={LandingPage} exact />
 				<Route path='/members' component={Members} exact />
 			</Switch>
+			<Footer />
 		</div>
 	);
 };
