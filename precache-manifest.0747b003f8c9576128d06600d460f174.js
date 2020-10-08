34c299f51a847994d82932581f7c8e594fcd3d3e
Sumit <ghewdesumit@gmail.com>
2020-10-07 21:07:51 -0400

    Updates

---
+++ precache-manifest.0747b003f8c9576128d06600d460f174.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1de6843eb8b64660b0bfa7974c1e89",
+    "url": "/CINSE-Website/index.html"
+  },
+  {
+    "revision": "187144fa497ed339ff09",
+    "url": "/CINSE-Website/static/css/main.d5609348.chunk.css"
+  },
+  {
+    "revision": "990b819a2ed980320bda",
+    "url": "/CINSE-Website/static/js/2.47f4706b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CINSE-Website/static/js/2.47f4706b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "187144fa497ed339ff09",
+    "url": "/CINSE-Website/static/js/main.d2117e49.chunk.js"
+  },
+  {
+    "revision": "f2a9dd4f7cc42b1cfdad",
+    "url": "/CINSE-Website/static/js/runtime-main.fa69de5e.js"
+  },
+  {
+    "revision": "3f11b24b9ec052b80d79cbb7eabf5bfb",
+    "url": "/CINSE-Website/static/media/CINSE.3f11b24b.png"
+  },
+  {
+    "revision": "4a0cf4a1484f9b77769b02bb43163a88",
+    "url": "/CINSE-Website/static/media/nan.4a0cf4a1.jpg"
+  },
+  {
+    "revision": "d2842fff46c9336990e3c3e481c78b16",
+    "url": "/CINSE-Website/static/media/stud2.d2842fff.png"
+  },
+  {
+    "revision": "62b2d73ff933e3f9ad62f389901c6042",
+    "url": "/CINSE-Website/static/media/stud3.62b2d73f.png"
+  }
+]);
