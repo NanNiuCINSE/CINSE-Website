18efe8bd7dbb055e3354e27426d2fd1f707c98a3
Sumit <ghewdesumit@gmail.com>
2020-10-16 15:41:11 -0400

    created contant us page

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './Contact.css';
+import nanNiuImage from '../../resources/Images/nan.jpg';
+
+const Contact = () => {
+	return (
+		<div className='contact-main-container'>
+			<div className='contact-wrapper'>
+				<header className='contact-header'>Contact Information</header>
+				<div className='contact-container'>
+					<div className='contact-image'>
+						<img src={nanNiuImage} alt='' />
+					</div>
+					<h2 className='contact-name'>
+						Nan Niu, <em>Ph.D</em>
+					</h2>
+					<h3 className='contact-designation'>Associate Professor</h3>
+
+					<div className='contact-info'>
+						<p className='contact-dept'>
+							<span className='dept-name'>
+								{' '}
+								Department of Electrical Engineering and Computer Science
+							</span>
+							<span className='dept-location'>
+								{' '}
+								832 Rhodes Hall, University of Cincinnati Cincinnati,
+							</span>{' '}
+							<span className='dept-zipcode'>OH 45221-0030 </span>
+						</p>
+						<div className='contact-options'>
+							<p>
+								{' '}
+								<i class='fas fa-phone-alt'></i> (513)-556 0051
+							</p>
+							<p>
+								{' '}
+								<i class='fas fa-envelope'></i> nan.niu@uc.edu
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Contact;
